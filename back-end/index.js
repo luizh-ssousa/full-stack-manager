390e3aea3ca0cff59866ac8f7d705af421477842
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-03-18 21:51:22 -0300

    if else

--- back-end/index.js
+++ back-end/index.js
@@ -9,32 +9,46 @@ app.use(express.json());
 mongoose.set("strictQuery", true);
 
 mongoose.connect("mongodb://localhost:27017/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const bookSchema = new mongoose.Schema({
-    title: String,
-    desc: String,
-    price: Number,
-    cover: String,
+  title: String,
+  desc: String,
+  price: Number,
+  cover: String,
 });
 
 const Book = mongoose.model("Book", bookSchema);
 
 app.get("/", (req, res) => {
-    res.json("Conectado jovem Padawan!");
+  res.json("Conectado jovem Padawan!");
 });
 
 app.get("/books", async (req, res) => {
-    try {
-        const books = await Book.find();
-        res.json(books);
-    } catch (err) {
-        res.status(580).json(err);
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(580).json(err);
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const books = await Book.findById(req.params.id);
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(400).json({ message: "Livro não encontrado" });
     }
+    res.json(books);
+  } catch (err) {
+    res.status(580).json(err);
+  }
 });
 
 app.listen(8800, () => {
-    console.log("Back-end conectado!");
+  console.log("Back-end conectado!");
 });
