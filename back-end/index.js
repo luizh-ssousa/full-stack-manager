f0cf6832126ac4f27c9e1e7c9a25b99f044a8f07
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-03-18 21:17:26 -0300

    mongoose

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+});
+
