60e2186cbc7cfe2ab9fa2d5ec1b86fa694b7409b
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-03-22 17:51:21 -0300

    css

--- front-end/books/src/App.jsx
+++ front-end/books/src/App.jsx
@@ -1,14 +1,14 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Books />}  />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Books />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ front-end/books/src/pages/Books/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Books = () => {
+  return (
+    <div className="ContainerBook">
+      <h1>Livraria Codando Loucamente</h1>
+    </div>
+  );
+};
+
+export default Books;
